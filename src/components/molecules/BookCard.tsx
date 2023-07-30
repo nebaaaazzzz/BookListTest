f03122a123f1a061a523455a0d5e1b301ee559cf
nebiyu <nebiyuf@gmail.com>
2023-07-30 14:05:16 +0300

    generate random image unique

--- src/components/molecules/BookCard.tsx
+++ src/components/molecules/BookCard.tsx
@@ -3,7 +3,10 @@ import { Book } from '../../interfaces/books.interface';
 export default function BookCard({ book }: { book: Book }) {
   return (
     <div className="max-w-xs">
-      <img className="max-w-full object-contain" src="https://picsum.photos/250/300" />
+      <img
+        className="max-w-full w-full aspect-[4/5] object-contain"
+        src={`https://picsum.photos/250/300?random=${Date.now()}`}
+      />
       <div className="px-4">
         <p className="text-ellipsis overflow-hidden whitespace-nowrap max-w-[150px]">{book.title}</p>
         <div className="flex justify-between">
