6271b6555661c5027c87930389752caa441184e1
nebiyu <nebiyuf@gmail.com>
2023-07-30 14:18:05 +0300

    axios instance

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+//json-server
+export const BASE_URL = 'http://localhost:3000';
--- src/service/books.ts
+++ src/service/books.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { Book } from '../interfaces/books.interface';
+import { BASE_URL } from '../constants';
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
 export const getBooks = async (page: number): Promise<Book[]> => {
-  const data = (await axios.get(`http://localhost:3000/books?_page=${page}`)).data;
+  const data = (await axiosInstance.get(`/books?_page=${page}`)).data;
   return data;
 };
