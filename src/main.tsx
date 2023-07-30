2c0874e6ad40dec64445105be9f31cb8444d168d
nebiyu <nebiyuf@gmail.com>
2023-07-30 13:58:19 +0300

    recoil add

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Books from './pages/books.page';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Books />
+      <RecoilRoot>
+        <Books />
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>,
 );
