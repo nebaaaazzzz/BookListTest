9cc1cef5173876a28fe2241a342d765d6ce33f0e
nebiyu <nebiyuf@gmail.com>
2023-07-30 14:15:50 +0300

    image with placeholder for loading

--- src/components/molecules/BookCard.tsx
+++ src/components/molecules/BookCard.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import { Book } from '../../interfaces/books.interface';
 
 export default function BookCard({ book }: { book: Book }) {
+  const [imageLoading, setImageLoading] = useState<boolean>(true);
+  const handleImageLoaded = () => setImageLoading(false);
   return (
     <div className="max-w-xs">
+      {imageLoading && (
+        <div className="animate-pulse bg-gray-200 max-w-full aspect-[4/5] object-contain w-[250px] h-[400px]"></div>
+      )}
       <img
+        onLoad={handleImageLoaded}
         className="max-w-full w-full aspect-[4/5] object-contain"
         src={`https://picsum.photos/250/300?random=${Date.now()}`}
       />
